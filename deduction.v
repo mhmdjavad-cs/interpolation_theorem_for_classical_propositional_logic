557b9e0315f7a201ede4c4aa6b7788eae75c758c
mhmdjavad <mhmdjavadcs@mhmdjavads-MacBook-Air.local>
2025-05-17 19:30:36 +0330

    deduction theorem and soundness theorem is added

--- deduction.v
+++ deduction.v
@@ -19,66 +19,80 @@ theorems for it as an axiom.
 
 Reserved Notation
   "Γ ⊢ p'"
-  (at level 40).
+  (at level 70).
 
 Inductive natural_deduction :
   context -> prop -> Prop :=
 
   (* when a formula belongs to the context, we can prove it. *)
   | axiom (Γ : context) (p : prop) :
-      p ∈ Γ -> Γ ⊢ p
+      p ∈ Γ ->
+      Γ ⊢ p
 
   (* left and elimination rule: *)
   | and_elim_left (Γ : context) (p q : prop) :
-      ( Γ ⊢ (And p q) ) -> Γ ⊢ p
+      Γ ⊢ (And p q) ->
+      Γ ⊢ p
 
   (* right elimination rule: *)
   | and_elim_right (Γ : context) (p q : prop) :
-       Γ ⊢ (And q p)  -> Γ ⊢ p
+      Γ ⊢ (And q p) ->
+      Γ ⊢ p
 
   (* and introduction rule: *)
   | and_intro (Γ : context) (p q: prop) :
-      (Γ ⊢ p) ∧ (Γ ⊢ q) -> Γ ⊢ (And p q)
+      Γ ⊢ p ->
+      Γ ⊢ q ->
+      Γ ⊢ (And p q)
 
   (* or elimination rule: *)
   | or_elim (Γ : context) (p A B : prop) :
-      ( Γ ⊢ (Or A B) ∧
-      (Γ ∪ {[A]}) ⊢ p ∧
-      (Γ ∪ {[B]}) ⊢ p)
-      -> (Γ ⊢ p)
+      Γ ⊢ (Or A B) ->
+      Γ ∪ {[A]} ⊢ p ->
+      Γ ∪ {[B]} ⊢ p ->
+      Γ ⊢ p
 
   (* left or introduction rule: *)
   | or_intro_left (Γ : context) (p q : prop) :
-      (Γ ⊢ p) -> (Γ ⊢ (Or p q))
+      Γ ⊢ p ->
+      Γ ⊢ (Or p q)
 
   (* left or introduction rule: *)
   | or_intro_right (Γ : context) (p q : prop) :
-      (Γ ⊢ p) -> (Γ ⊢ (Or q p))
+      Γ ⊢ p ->
+      Γ ⊢ (Or q p)
 
   (* implication elimination rule: *)
   | imp_elim (Γ : context) (p q : prop) :
-      (Γ ⊢ q) ∧ (Γ ⊢ (Imp q p)) -> (Γ ⊢ p)
+      Γ ⊢ q ->
+      Γ ⊢ (Imp q p) ->
+      Γ ⊢ p
 
   (* implication introduction rule: *)
   | imp_intro (Γ : context) (p q : prop):
-      ((Γ ∪ {[q]}) ⊢ p) -> (Γ ⊢ (Imp q p))
+      Γ ∪ {[q]} ⊢ p ->
+      Γ ⊢ (Imp q p)
 
   (* negation elimination rule: *)
   | neg_elim (Γ : context) (p : prop) :
-      (Γ ⊢ p) ∧ (Γ ⊢ (Neg p)) ->
-      (Γ ⊢ Bot)
+      Γ ⊢ p ->
+      Γ ⊢ (Neg p) ->
+      Γ ⊢ Bot
 
   (* negation introduction rule: *)
   | neg_intro (Γ : context) (p : prop) :
-      ((Γ ∪ {[p]}) ⊢ Bot) -> (Γ ⊢ (Neg p))
+      Γ ∪ {[p]} ⊢ Bot ->
+      Γ ⊢ (Neg p)
 
   (* reducto ad absurdum rule: *)
   | RAA (Γ : context) (p : prop):
-      ((Γ ∪ {[Neg p]}) ⊢ Bot) -> (Γ ⊢ p)
+      Γ ∪ {[Neg p]} ⊢ Bot ->
+      Γ ⊢ p
 
   (* bot elimination rule: *)
   | bot_elim (Γ : context) (p : prop) :
-      (Γ ⊢ Bot) -> (Γ ⊢ p)
+      Γ ⊢ Bot ->
+      Γ ⊢ p
 
   (* top introduction rule: *)
   | top_intro (Γ : context) :
@@ -121,29 +135,28 @@ Qed.
 
 Example nd3 : forall p q : prop, {[p ; q]} ⊢ (And p q).
 Proof with auto.
-intros. apply and_intro. split...
+intros. apply and_intro...
 Qed.
 
 Example nd4 : forall p q : prop, {[p ; Neg p]} ⊢ q.
 Proof with auto.
 intros.
-apply bot_elim. apply neg_elim with (p := p).
-split...
+apply bot_elim. apply neg_elim with (p := p)...
 Qed.
 
 Example nd5 : forall p : prop, ∅ ⊢ Imp p (Neg (Neg p)).
 Proof with auto.
 intros. apply imp_intro. apply neg_intro.
-apply neg_elim with (p := p). split...
+apply neg_elim with (p := p)...
 Qed.
 
 Example nd6 : forall p : prop, ∅ ⊢ Or p (Neg p).
 Proof with auto.
-intros. apply RAA. apply neg_elim with (p := Or p (Neg p)). split.
+intros. apply RAA. apply neg_elim with (p := Or p (Neg p)).
 - apply or_intro_right. apply neg_intro.
-apply imp_elim with (q := Or p (Neg p)). split.
+apply imp_elim with (q := Or p (Neg p)).
   + apply or_intro_left. in_context.
-  + apply imp_intro. apply neg_elim with ( p := Or p (Neg p)). split...
+  + apply imp_intro. apply neg_elim with ( p := Or p (Neg p))...
 - in_context.
 Qed.
 
@@ -162,21 +175,41 @@ inversion H.
 
 
 
+Theorem weakening (Γ : context) (A B : prop):
+  (Γ ⊢ A) -> (Γ ∪ {[B]} ⊢ A).
+Proof.
+assert (set_swap: forall (Γ : context) (A B : prop) , Γ ∪ {[A]} ∪ {[B]} = Γ ∪ {[B]} ∪ {[A]} ).
+{intros. set_solver. }
+intros.
+induction H.
+-apply axiom. apply elem_of_union_l. apply H.
+-eapply and_elim_left. apply IHnatural_deduction.
+-eapply and_elim_right. apply IHnatural_deduction.
+-apply and_intro; assumption.
+-eapply or_elim with (A := A). apply IHnatural_deduction1. rewrite set_swap; assumption.
+rewrite set_swap. assumption.
+-apply or_intro_left. assumption.
+-apply or_intro_right. assumption.
+-eapply imp_elim. apply IHnatural_deduction1. assumption.
+-apply imp_intro. rewrite set_swap. assumption.
+-eapply neg_elim. apply IHnatural_deduction1. assumption.
+-apply neg_intro. rewrite set_swap. assumption.
+-apply RAA. rewrite set_swap. assumption.
+-apply bot_elim. assumption.
+-apply top_intro.
+Qed.
+
+
+
 (* deduction theorem *)
 Theorem deduction_theorem (Γ : context) (A B : prop):
   ((Γ ∪ {[A]}) ⊢ B) <-> Γ ⊢ Imp A B.
 Proof.
 split.
 -intros. apply imp_intro. assumption.
--intros. inversion H.
-  +apply imp_elim with (q:=A). split. in_context. apply axiom.
-  apply elem_of_union. left. assumption.
-  +
-  Admitted.
-
-
-
-
+-intros. apply imp_elim with (q:= A).
+in_context. apply weakening. apply H.
+Qed.
 
 (* the soundness theorems for or deduction system*)
 
@@ -185,7 +218,45 @@ Theorem soundness1 (Γ : context) (p : prop) :
 Proof.
 unfold model3. unfold model2. unfold model1.
 intros.
-Admitted.
+induction H.
+-apply H0. apply H.
+-apply IHnatural_deduction in H0. simpl in H0. apply andb_true_iff in H0. apply H0.
+-apply IHnatural_deduction in H0. simpl in H0. apply andb_true_iff in H0. apply H0.
+-simpl. apply andb_true_iff. split; auto.
+-assert (H3 := H0).
+apply IHnatural_deduction1 in H0. simpl in H0. apply orb_true_iff in H0. destruct H0.
+  +apply IHnatural_deduction2. intros. apply elem_of_union in H4. destruct H4.
+    --apply H3. assumption.
+    --apply elem_of_singleton in H4. rewrite H4. assumption.
+  +apply IHnatural_deduction3. intros. apply elem_of_union in H4. destruct H4.
+    --apply H3. assumption.
+    --apply elem_of_singleton in H4. rewrite H4. assumption.
+-simpl. apply orb_true_iff. left. auto.
+-simpl. apply orb_true_iff. right. auto.
+-simpl in IHnatural_deduction2. destruct (interpret I q) eqn:m.
+  +simpl in IHnatural_deduction2. apply IHnatural_deduction2. assumption.
+  +apply IHnatural_deduction1 in H0. discriminate.
+-simpl. destruct (interpret I q) eqn:m.
+  +simpl. apply IHnatural_deduction. intros. apply elem_of_union in H1. destruct H1.
+    --auto.
+    --apply elem_of_singleton in H1. rewrite H1. auto.
+  +simpl. reflexivity.
+-assert (H3 := H0). apply IHnatural_deduction1 in H0. apply IHnatural_deduction2 in H3.
+simpl in H3. rewrite H0 in H3. discriminate.
+-simpl. simpl in IHnatural_deduction. destruct (interpret I p) eqn:m.
+  +simpl. apply IHnatural_deduction. intros. apply elem_of_union in H1. destruct H1.
+    --auto.
+    --apply elem_of_singleton in H1. rewrite H1. auto.
+  +reflexivity.
+-destruct (interpret I p) eqn:m. reflexivity. simpl in IHnatural_deduction.
+apply IHnatural_deduction. intros. apply elem_of_union in H1. destruct H1.
+  +auto.
+  +apply elem_of_singleton in H1. rewrite H1. simpl. rewrite m. reflexivity.
+-destruct (interpret I p) eqn:m. reflexivity. simpl in IHnatural_deduction. apply IHnatural_deduction.
+intros. apply H0. assumption.
+-reflexivity.
+Qed.
+
 
 Theorem soundness2 (p : prop) :
   ∅ ⊢ p -> ⊨₀ p.
